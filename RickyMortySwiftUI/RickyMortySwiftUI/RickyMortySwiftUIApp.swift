e7cfc955545c7472542ad50201698884395a10ad
Pablo Ureña Simón <pablourena10@gmail.com>
2025-09-18 16:41:44 +0200

    creation of the project

---
+++ RickyMortySwiftUI/RickyMortySwiftUI/RickyMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortySwiftUIApp.swift
+//  RickyMortySwiftUI
+//
+//  Created by Pablo on 18/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
